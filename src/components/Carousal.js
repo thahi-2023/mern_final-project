15d5fc0665976e004215b3b64baafe78c980b9af
unknown <thahira.f2021@gmail.com>
2023-09-07 16:01:03 -0400

    added Carousal and Card to the body

---
+++ src/components/Carousal.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function Carousal() {
+  return (
+    <div>
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="
+      https://source.unsplash.com/random/900x700/?momos" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="https://source.unsplash.com/random/900x700/?fries" className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+       
+     
+     
+      
+      </div>
+   
+    
+  )
+}
+
+export default Carousal
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,35 +1,23 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousal from '../components/Carousal'
 
 function Home() {
-  return (
-    <div>
-      <div> <Navbar/> </div>  
-
-      <div>
-        <div className="card mt-3" style={{"width": "18rem", "maxHeight": "360"}}>
-            <img src="..." className="card-img-top" alt="..."/>
-            <div classNmae="card-body">
-                <h5 className="card-title">Card Title</h5>
-                <p className="card-text">Text</p>
-                <div className='container w-100'></div>
-                <select classname='m-2 h-100 bg-success'>
-                    {Array.from(Array(5), (e,i)=> {
-                        return(
-                            <option key={i+1} value={i+1}> {i+1}</option>
-                        )
-                    })}
-                </select>
-
+    return (
+        <div>
+            <div> <Navbar /> </div>
+            <div> <Carousal/></div>
+            <div>
+                <Card />
             </div>
 
-        </div>
-      </div>
-      <div> <Footer/> </div>
 
-    </div>
-  )
+            <div> <Footer /> </div>
+
+        </div>
+    )
 }
 
 export default Home
