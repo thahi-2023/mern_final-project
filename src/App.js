43ee0034ee584f5350fe43dc124cdccc539b677a
unknown <thahira.f2021@gmail.com>
2023-09-07 12:33:36 -0400

    added Navbar

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import Login from './screens/Login';
 
 function App() {
   return (
     <Router>
     <div> 
       <Routes>
-        
+        <Route exact path='/' element={<Home/>}/> 
+        <Route exact path='/login' element={<Login/>}/>
       </Routes>
     </div>
     </Router>
