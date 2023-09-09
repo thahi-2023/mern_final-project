ebda612e20d0aabefca1a4951d547e7bdf1ef090
unknown <thahira.f2021@gmail.com>
2023-09-09 18:22:33 -0400

    connected mongoDb

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,33 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://thahiraf2021:Inshirahf2021@sei.qvnn4w4.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://thahiraf2021:Inshirahf2021@sei.qvnn4w4.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 
 
 //connecting mongoDB
 const mongoDB = () => {
    mongoose.connect(mongoURI)
    .then(()=>console.log("connected successfully"))
+   
    .catch((err)=>{
+      //get data from mongodb
+      const fetched_data =  mongoose.connection.db.collection("food_items")
+   fetched_data.find({}).toArray(function( err, data){
+    if (err) console.log(err);
+    else console.log(data);
+   })
 
    })
 }
  //await mongoose.connect(mongoURI,()=>{
 //console.log("connected")
 //})
-
+// const mongoDB =async() =>{
+//     await mongoose.connect(mongoURI,{ useNewUrlParser: true }, async(err,result)=>{
+//         if(err) console.log("---",err)
+//         else{
+//     console.log ("connected")
+// }
+//     })
+// }
 
 
 
