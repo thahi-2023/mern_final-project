0eccd5c7adead3161b97f916bbac457fa8127f52
unknown <thahira.f2021@gmail.com>
2023-09-11 07:39:38 -0400

    Validate the user models

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://thahiraf2021:Inshirahf2021@sei.qvnn4w4.mongodb.net/gofoodmern?retryWrites=true&w=majority'
+const mongoURI ='mongodb+srv://thahiraf2021:Inshirahf2021@sei.qvnn4w4.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 
+//mongodb+srv://thahiraf2021:<password>@sei.qvnn4w4.mongodb.net/?retryWrites=true&w=majority
 
 //connecting mongoDB
 const mongoDB = () => {
@@ -8,26 +9,30 @@ const mongoDB = () => {
    .then(()=>console.log("connected successfully"))
    
    .catch((err)=>{
-      //get data from mongodb
-      const fetched_data =  mongoose.connection.db.collection("sample")
-   fetched_data.find({}).toArray(function( err, data){
-    if (err) console.log(err);
-    else console.log();
-   })
+     // get data from mongodb
+   //   const fetched_data =  mongoose.connection.db.collection("user")
+   //    fetched_data.find({}).toArray(function( err, data){
+   //     if (err) console.log(err);
+   //    else console.log();
+   //    })
 
    })
 }
- //await mongoose.connect(mongoURI,()=>{
-//console.log("connected")
-//})
+ 
+
 // const mongoDB =async() =>{
 //     await mongoose.connect(mongoURI,{ useNewUrlParser: true }, async(err,result)=>{
 //         if(err) console.log("---",err)
 //         else{
 //     console.log ("connected")
+// const fetched_data =  mongoose.connection.db.collection("user")
+// fetched_data.find({}).toArray(function( err, data){
+//  if (err) console.log(err);
+// else console.log();
+// })
 // }
 //     })
-// }
+//  }
 
 
 
