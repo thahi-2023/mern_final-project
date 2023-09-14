657794d1ad43443bdfa11a4291a7bb39ba38b200
unknown <thahira.f2021@gmail.com>
2023-09-13 21:43:31 -0400

    Setting logout and My order

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,17 +12,30 @@ import {Link} from 'react-router-dom';
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
+      <ul className="navbar-nav me-auto mb-2">
         <li className="nav-item">
           <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
         </li>
+        {/* if auth token exis show my order  */}
+        {(localStorage.getItem("authToken"))?
+         <li className="nav-item">
+         <Link className="nav-link active fs-5" aria-current="page" to="/">My Orders</Link>
+       </li>
+         : ""}
         
          </ul>
+         {/* show my login and signup page only authToken exit */}
+         {(!localStorage.getItem("authToken"))?
          
          <div className='d-flex'>
           <Link className="btn bg-white text-success mx-1" to="/login">Login</Link>
          <Link className="btn bg-white text-success mx-1" to="/creatuser">SignUp</Link>
          </div>
+         : 
+         <div className="btn bg-white text-success mx-2">
+          Logout
+         </div>
+         }
     </div>
   </div>
 </nav>
